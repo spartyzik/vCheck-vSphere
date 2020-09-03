3439503f05b10af2f027f02655381825c04305b9
Symanzik, Ed <Ed.Symanzik@gmail.com>
2020-09-03 08:35:30 -0400

    CPU Hot _Add_ enabled

--- Plugins/60 VM/201 VMs Hot Add Configuration.ps1
+++ Plugins/60 VM/201 VMs Hot Add Configuration.ps1
@@ -6,7 +6,7 @@ $PluginVersion = 1.2
 $PluginCategory = "vSphere"
 
 # Start of Settings 
-# Should CPU hot plug be enabled
+# Should CPU hot add be enabled
 $CPUHotAdd = $false
 # Should Memory hot add be enabled
 $MEMHotAdd = $false
@@ -16,13 +16,14 @@ $MEMHotAdd = $false
 $CPUHotAdd = Get-vCheckSetting $Title "CPUHotAdd" $CPUHotAdd
 $MEMHotAdd = Get-vCheckSetting $Title "MEMHotAdd" $MEMHotAdd
 
-$VMs | Select-Object Name, @{N="CPU Hot Plug Enabled"; E={$_.ExtensionData.config.CpuHotAddEnabled}}, @{N="Memory Hot Add Enabled"; E={$_.ExtensionData.config.MemoryHotAddEnabled}} | Where-Object {$_."CPU Hot Plug Enabled" -ne $CPUHotAdd -or $_."Memory Hot Add Enabled" -ne $MEMHotAdd}
+$VMs | Select-Object Name, @{N="CPU Hot Add Enabled"; E={$_.ExtensionData.config.CpuHotAddEnabled}}, @{N="Memory Hot Add Enabled"; E={$_.ExtensionData.config.MemoryHotAddEnabled}} | 
+       Where-Object {($_."CPU Hot Add Enabled" -ne $CPUHotAdd) -or ($_."Memory Hot Add Enabled" -ne $MEMHotAdd)}
 
 # Create variables with unexpected values, for use in the plugin comment
 $CPUNotExpected = if ($CPUHotAdd) { "disabled" } else { "enabled" }
 $MEMNotExpected = if ($MEMHotAdd) { "disabled" } else { "enabled" }
 
-$Comments = ("The following lists all VMs with CPU hot plug {0} or Memory hot add {1}" -f $CPUNotExpected, $MEMNotExpected)
+$Comments = ("The following lists all VMs with CPU hot add {0} or Memory hot add {1}" -f $CPUNotExpected, $MEMNotExpected)
 
 # Change Log
 ## 1.2 : Added Get-vCheckSetting
